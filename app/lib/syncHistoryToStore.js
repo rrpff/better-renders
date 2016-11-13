c5d4bda372ce9053cf7af4e25739e4ac7ee7e66a
Richard Foster <richard@madetech.co.uk>
2016-11-13 22:45:29 +0000

    Dispatch SET_LOCATION when page loads

--- app/lib/syncHistoryToStore.js
+++ app/lib/syncHistoryToStore.js
@@ -1,6 +1,8 @@
 const { setLocation } = require('./actions/routing')
 
 module.exports = function syncHistoryToStore (history, store) {
+  store.dispatch(setLocation(history.location))
+
   history.listen(location => {
     store.dispatch(setLocation(location))
   })
--- test/lib/syncHistoryToStore.js
+++ test/lib/syncHistoryToStore.js
@@ -3,22 +3,29 @@ const createMemoryHistory = require('history/createMemoryHistory').default
 const syncHistoryToStore = require('../../app/lib/syncHistoryToStore')
 const { SET_LOCATION } = require('../../app/lib/types')
 
-describe('Sync History To Store', function () {
-  it('should dispatch a SET_LOCATION action on history changes', function () {
-    const history = createMemoryHistory()
-    const store = configureMockStore()({})
-    syncHistoryToStore(history, store)
+function setup () {
+  const history = createMemoryHistory()
+  const store = configureMockStore()({})
+  syncHistoryToStore(history, store)
 
-    const location = {
+  return { history, store }
+}
+
+describe('Sync History To Store', function () {
+  it('should dispatch a SET_LOCATION action when called and on history changes', function () {
+    const { history, store } = setup()
+
+    history.push({
       pathname: '/test',
       search: '?query=true',
       state: { test: 'state' }
-    }
+    })
 
-    history.push(location)
+    const [first, second] = store.getActions()
 
-    const action = store.getActions()[0]
-    expect(action.type).to.eq(SET_LOCATION)
-    expect(action.location.pathname).to.eq(location.pathname)
+    expect(first.type).to.eq(SET_LOCATION)
+    expect(first.location.pathname).to.eq('/')
+    expect(second.type).to.eq(SET_LOCATION)
+    expect(second.location.pathname).to.eq('/test')
   })
 })
