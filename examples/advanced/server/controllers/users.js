316ef3e7fb028524aa5313c5cf61eb7f2e57af55
Richard Foster <richard@madetech.co.uk>
2016-11-22 16:48:38 +0000

    Simply does it

--- examples/advanced/server/controllers/users.js
+++ examples/advanced/server/controllers/users.js
@@ -2,23 +2,14 @@ const { Router } = require('express')
 
 const router = new Router()
 
-const models = [{ name: 'Storm', username: 'storm' }, { name: 'Richard', username: 'richard' }]
+const users = [{ name: 'Storm', username: 'storm' }, { name: 'Richard', username: 'richard' }]
 
 router.get('/users', async function (req, res) {
-  const users = models.map(user => ({
-    name: user.name,
-    username: user.username
-  }))
-
   res.better.render('UserIndexPage', { users })
 })
 
 router.get('/users/:username', async function (req, res) {
-  const model = models.find(user => user.username === req.params.username)
-  const user = {
-    name: model.name,
-    username: model.username
-  }
+  const user = users.find(model => model.username === req.params.username)
 
   res.better.render('UserShowPage', { user })
 })
