c3bd1696bd025f803fce23fa69df16a6beb12074
Richard Foster <richard@madetech.co.uk>
2016-11-22 16:45:37 +0000

    Cleanup

--- examples/advanced/server/controllers/users.js
+++ examples/advanced/server/controllers/users.js
@@ -2,30 +2,23 @@ const { Router } = require('express')
 
 const router = new Router()
 
-const USERS = [{ name: 'Storm', username: 'storm' }, { name: 'Richard', username: 'richard' }]
+const models = [{ name: 'Storm', username: 'storm' }, { name: 'Richard', username: 'richard' }]
 
 router.get('/users', async function (req, res) {
-  const models = USERS
-  console.log(models)
   const users = models.map(user => ({
     name: user.name,
     username: user.username
   }))
 
-  console.log(users)
-
   res.better.render('UserIndexPage', { users })
 })
 
 router.get('/users/:username', async function (req, res) {
-  console.log('here')
-  const model = USERS.find(user => user.username === req.params.username)
-  console.log(model)
+  const model = models.find(user => user.username === req.params.username)
   const user = {
     name: model.name,
     username: model.username
   }
-  console.log(user)
 
   res.better.render('UserShowPage', { user })
 })
