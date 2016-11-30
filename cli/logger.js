157c994fd7a2c9104b97f4c0e4dd4fc4c0d6729d
Richard Foster <richard@madetech.co.uk>
2016-11-30 23:14:00 +0000

    Make logs prettier

--- cli/logger.js
+++ cli/logger.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk')
 const ora = require('ora')
 
-exports.success = message => console.log(chalk.green(message))
-exports.warn = message => console.warn(chalk.yellow(message))
-exports.error = message => console.error(chalk.red(message))
+exports.success = message => console.log(`${chalk.green('success')} ${message}`)
+exports.warn = message => console.warn(`${chalk.yellow('warning')} ${message}`)
+exports.error = message => console.error(`${chalk.red('error')} ${message}`)
 exports.spinner = message => ora(message).start()
