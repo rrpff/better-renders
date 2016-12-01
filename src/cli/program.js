ab520d9caac0c8f4838d8e811e5f3a53f257c494
Richard Foster <richard@madetech.co.uk>
2016-12-01 21:54:12 +0000

    Add --version back to CLI

--- src/cli/program.js
+++ src/cli/program.js
@@ -1,9 +1,12 @@
 const program = require('commander')
+const pkg = require('../../package.json')
 const create = require('./commands/create')
 const compile = require('./commands/compile')
 const start = require('./commands/start')
 const watch = require('./commands/watch')
 const withErrors = require('./helpers/withErrors')
+
+program.version(pkg.version)
 
 program
   .command('new <name> [dir]')
